3d8c491206ec194add5ee4bd66b6a74916afb4cc
zouhang <1102639563@qq.com>
2022-10-14 15:39:23 +0800

    feat: support some command

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,174 @@ import yargs from "yargs"
 import fs from "fs-extra"
 import path from "path"
 import chalk from "chalk"
-import ora from "ora"
 import { prompt } from "enquirer"
+
+import { getModuleProjects, downloadFisand, checkFisandComp } from "./utils"
+
+const cwd = process.cwd()
+let CONFIG_FILE = "fisand.json"
+let root = path.join(cwd, "fisand-modules")
 
 yargs
   .scriptName("fisand")
   .locale("en-US")
   .usage("$0 <command> [options]")
-  .command('list', chalk.bold.yellow('list all available sources'), async (yargs) => {
-    const response = await prompt({
-      type: 'input',
-      name: 'name',
-      message: 'Who is the most beautiful person in the world?'
-    });
-    
-    console.log(response);
-  })
-  .example("$0 list", chalk.bold.greenBright('list all available sources'))
+  .command(
+    "list",
+    chalk.bold.yellow("List all available sources"),
+    checkFisandComp,
+  )
+  .command(
+    "workspace [workspace]",
+    "Init source repo",
+    async (_) => {
+      _.positional("workspace", {
+        type: "string",
+        describe: "Source repo",
+      })
+    },
+    async (argv) => {
+      let workspace = argv.workspace
+      const projects = await getModuleProjects()
+      const workspaces = projects.map((project) => {
+        return {
+          name: project.name,
+          message: `${project.name}(${project.description})`,
+        }
+      })
+      const isWorkspaceValid =
+        workspace && workspaces.find((project) => project.name === workspace)
+
+      if (!isWorkspaceValid) {
+        workspace && console.log(chalk.red.bold(`${workspace} is not exist\n`))
+
+        if (!workspace) {
+          const { project } = await prompt<{
+            project: string
+          }>({
+            type: "select",
+            name: "project",
+            message: "Please Select",
+            choices: workspaces,
+          })
+          workspace = project
+        }
+      }
+      // make fisand.json
+      const fisandConfFile = path.join(cwd, CONFIG_FILE)
+      const confTemplate = {
+        config: {
+          workspace,
+        },
+      }
+
+      fs.writeFileSync(fisandConfFile, JSON.stringify(confTemplate, null, 2))
+
+      console.log(chalk.green(`Init ${CONFIG_FILE} success`))
+    },
+  )
+  .command(
+    "import [-d] [-r] <name..>",
+    "Import source code",
+    async (_) => {
+      _.positional("name", {
+        type: "string",
+        describe: "Source name",
+      })
+      _.positional("directory", {
+        type: "string",
+        describe: "Download path",
+        alias: "d",
+      })
+      _.positional("ref", {
+        type: "string",
+        describe: "Source version: branch/commit/tag",
+        alias: "r",
+        default: "master",
+      })
+    },
+    async (
+      argv: yargs.ArgumentsCamelCase<{
+        name: string[]
+        r?: string
+        ref?: string
+        d?: string
+        directory: string
+      }>,
+    ) => {
+      const fisandComps = await checkFisandComp()
+      argv.name.forEach((n) => {
+        if (!fisandComps.includes(n)) {
+          console.log(
+            chalk.red.bold(`${n} is not exist\nUse fisand list to check`),
+          )
+          process.exit(1)
+        }
+      })
+
+      // The name of branch, tag or commit
+      let ref = argv.r || argv.ref || "master"
+      const fisandConfFile = path.join(cwd, CONFIG_FILE)
+      const confTemplate: Record<string, any> = {}
+      if (!fs.existsSync(fisandConfFile)) {
+        argv.name.forEach((n) => (confTemplate[n] = ref))
+        fs.writeFileSync(fisandConfFile, JSON.stringify(confTemplate, null, 2))
+      } else {
+        const existConf = require(fisandConfFile)
+        if (existConf && existConf.config && existConf.config.workspace) {
+          root = path.join(cwd, existConf.config.workspace)
+        }
+        // get workspace
+        argv.name.forEach((n) => {
+          if (typeof existConf[n] === "string") {
+            ref = existConf[n]
+          } else {
+            existConf[n] = ref
+            fs.writeFileSync(fisandConfFile, JSON.stringify(existConf, null, 2))
+          }
+        })
+      }
+
+      const notBoolean = (...b: unknown[]) =>
+        b.every((_) => typeof _ !== "boolean")
+      // dest
+      if ((argv.d || argv.directory) && notBoolean(argv.d, argv.directory)) {
+        root = path.join(cwd, argv.d || argv.directory)
+      }
+
+      // order -d > fisand.json workspace > fisand-modules
+      console.log(chalk.green(`Download source to ${root}`))
+
+      if (!fs.existsSync(root)) {
+        fs.mkdirSync(`${root}`, { recursive: true })
+      }
+
+      for (const i of argv.name.map((n) =>
+        downloadFisand(n, notBoolean(ref) ? ref : "master"),
+      )) {
+        // download source
+        const depMap = await i
+
+        // search deps
+        const deps = Object.keys(depMap)
+        const workspace = (require(fisandConfFile).config || {}).workspace || ""
+        if (workspace) {
+          for (const dep of deps) {
+            if (dep.startsWith(workspace)) {
+              const name = dep.replace(`${workspace}/`, "")
+              // check deps then download
+              if (!fs.existsSync(`${root}/${name}`)) {
+                await downloadFisand(name, (depMap[dep] as string) || "master")
+              }
+            }
+          }
+        }
+      }
+
+      process.exit(0)
+    },
+  )
+  .example("$0 list", chalk.bold.greenBright("List all available sources"))
   .help()
   .alias("help", "h")
   .version()
--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,17 @@
-import fs from 'fs'
-import path from 'path'
-import ora from 'ora'
-import chalk from 'chalk'
-import { Repositories, RepositoryFiles, Groups } from '@gitbeaker/node'
+import fs from "fs"
+import path from "path"
+import ora from "ora"
+import chalk from "chalk"
+import { Repositories, RepositoryFiles, Groups } from "@gitbeaker/node"
 
 const cwd = process.cwd()
 
-const PRIVATE_TOKEN = ''
-const GROUP_ID = ''
-let REPO_ID: string | number = ''
-const CONFIG_FILE = 'fisand.json'
-
-const conf = {
-  token: PRIVATE_TOKEN,
-  host: '',
+let CONFIG_FILE = "fisand.json"
+let GROUP_ID = ""
+let REPO_ID: string | number = ""
+let conf = {
+  token: "",
+  host: "",
 }
 
 async function getLocalConfig() {
@@ -21,16 +19,14 @@ async function getLocalConfig() {
   if (fs.existsSync(fisandConfFile)) {
     const existConf = require(fisandConfFile)
     if (existConf.config) {
-      const project_id = await getProjectId(
-        existConf.config.workspace
-      )
-      REPO_ID = project_id || '174837'
+      const project_id = await getProjectId(existConf.config.workspace)
+      REPO_ID = project_id || ""
+      conf.token = existConf.config.token
+      GROUP_ID = existConf.config.group_id
     }
   } else {
     console.log(
-      chalk.red.bold(
-        `${CONFIG_FILE}不存在\n使用 fisand workspace 初始化配置`
-      )
+      chalk.red.bold(`${CONFIG_FILE} is not exist\nUse fisand workspace to init config`),
     )
     process.exit(1)
   }
@@ -39,72 +35,61 @@ async function getLocalConfig() {
 function getLocalConfigSync() {
   const fisandConfFile = path.join(cwd, CONFIG_FILE)
 
-  return fs.existsSync(fisandConfFile)
-    ? require(fisandConfFile)
-    : {}
+  return fs.existsSync(fisandConfFile) ? require(fisandConfFile) : {}
 }
 
 async function downloadFisand(
   moduleName: string,
   dest: string,
-  ref = 'master'
+  ref = "master",
 ) {
-  const spinner = ora('Downloading...')
+  const spinner = ora("Downloading...")
   spinner.start()
 
-  const deps = await genFilesAndFolders(
-    moduleName,
-    dest,
-    ref
-  )
+  const deps = await genFilesAndFolders(moduleName, dest, ref)
 
-  spinner.succeed('Downloading success')
+  spinner.succeed("Download success")
 
-  return deps
+  return deps as Record<string, string>
 }
 
-async function genFilesAndFolders(moduleName: string, dest: string, ref: string) {
+async function genFilesAndFolders(
+  moduleName: string,
+  dest: string,
+  ref: string,
+) {
   const tree = await getRepoTree(moduleName)
   let deps = {}
   if (tree && tree.length) {
     for (const d of tree) {
-      if (d.type === 'tree') {
+      if (d.type === "tree") {
         fs.mkdirSync(`${dest}/${d.name}`, {
           recursive: true,
         })
         await genFilesAndFolders(
           `${moduleName}/${d.name}`,
           `${dest}/${d.name}`,
-          ref
+          ref,
         )
-      } else if (d.type === 'blob') {
+      } else if (d.type === "blob") {
         try {
           const excludes = getLocalConfigSync().excludes
           const ignore = (path: string) => excludes.includes(path)
 
           if (
             !excludes
-              ? d.path.includes('tsconfig.json') ||
-                d.path.includes('package.json')
+              ? d.path.includes("tsconfig.json") ||
+                d.path.includes("package.json")
               : ignore(path.basename(d.path))
           ) {
-            if (d.path.includes('package.json')) {
-              const content = await downloanFile(
-                `${d.path}`,
-                '',
-                ref,
-                false
-              )
+            if (d.path.includes("package.json")) {
+              const content = await downloadFile(`${d.path}`, "", ref, false)
               deps = JSON.parse(content as string).fisand || {}
             } else {
               await Promise.resolve()
             }
           } else {
-            await downloanFile(
-              `${d.path}`,
-              `${dest}/${d.name}`,
-              ref
-            )
+            await downloadFile(`${d.path}`, `${dest}/${d.name}`, ref)
           }
         } catch (error) {
           console.log(error)
@@ -115,7 +100,7 @@ async function genFilesAndFolders(moduleName: string, dest: string, ref: string)
   return deps
 }
 
-async function getRepoTree(path: string, ref = 'master') {
+async function getRepoTree(path: string, ref = "master") {
   const repo = new Repositories(conf)
   const tree = await repo.tree(REPO_ID, {
     path: `packages/${path}`,
@@ -124,38 +109,32 @@ async function getRepoTree(path: string, ref = 'master') {
   return tree
 }
 
-async function downloanFile(
+async function downloadFile(
   filePath: string,
   dest: string,
-  ref = 'master',
-  write = true
+  ref = "master",
+  write = true,
 ) {
   const repoFile = new RepositoryFiles(conf)
-  const { content } = await repoFile.show(
-    REPO_ID,
-    filePath,
-    ref
-  )
+  const { content } = await repoFile.show(REPO_ID, filePath, ref)
+
   return new Promise((resolve, reject) => {
     write
-      ? fs.writeFile(
-          dest,
-          Buffer.from(content, 'base64'),
-          (error) => {
-            if (!error) {
-              resolve(true)
-            } else {
-              reject(error)
-            }
+      ? fs.writeFile(dest, Buffer.from(content, "base64"), (error) => {
+          if (!error) {
+            resolve(true)
+          } else {
+            reject(error)
           }
-        )
-      : resolve(
-          Buffer.from(content, 'base64').toString('utf-8')
-        )
+        })
+      : resolve(Buffer.from(content, "base64").toString("utf-8"))
   })
 }
 
 async function getModuleProjects() {
+  if (!conf.token || !GROUP_ID) {
+    throw Error("Please set your gitlab token and repo group")
+  }
   const group = new Groups(conf)
   const projects = await group.projects(GROUP_ID)
 
@@ -164,16 +143,28 @@ async function getModuleProjects() {
 
 async function getProjectId(workspace: string) {
   const projects = await getModuleProjects()
-  const project = projects.find(
-    (project) => project.name === workspace
-  )
+  const project = projects.find((project) => project.name === workspace)
 
   return project?.id
 }
 
-module.exports = {
+async function checkFisandComp() {
+  try {
+    const spinner = ora("Loading...\n")
+    spinner.start()
+    await getLocalConfig()
+    const tree = await getRepoTree("")
+    spinner.succeed()
+    return tree.filter((t) => t.type === "tree").map((t) => t.name)
+  } catch (error) {
+    process.exit(1)
+  }
+}
+
+export {
   downloadFisand,
   getRepoTree,
   getModuleProjects,
   getLocalConfig,
+  checkFisandComp,
 }
