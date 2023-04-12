a3711500bd80a4d133b3aebebe6bc2369fb687fa
zouhang <1102639563@qq.com>
2023-04-12 22:35:52 +0800

    chore: update code

--- src/index.ts
+++ src/index.ts
@@ -1,176 +1,79 @@
-import yargs from "yargs"
-import fs from "fs-extra"
-import path from "path"
-import chalk from "chalk"
-import { prompt } from "enquirer"
+import * as p from "@clack/prompts"
+import color from "picocolors"
+import { exec as exec_ } from "node:child_process"
+import { promisify } from "node:util"
 
-import { getModuleProjects, downloadFisand, checkFisandComp } from "./utils"
+const exec = promisify(exec_);
 
-const cwd = process.cwd()
-let CONFIG_FILE = "fisand.json"
-let root = path.join(cwd, "fisand-modules")
+async function runCommand(cmd: string) {
+  try {
+    const {stdout, stderr} = await exec(cmd);
+    console.log(stdout);
+    console.error(stderr);
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
+}
 
-yargs
-  .scriptName("fisand")
-  .locale("en-US")
-  .usage("$0 <command> [options]")
-  .command(
-    "list",
-    chalk.bold.yellow("List all available sources"),
-    checkFisandComp,
-  )
-  .command(
-    "workspace [workspace]",
-    "Init source repo",
-    async (_) => {
-      _.positional("workspace", {
-        type: "string",
-        describe: "Source repo",
-      })
+async function main() {
+  console.clear()
+
+  p.intro(`${color.bgCyan(color.black(" fisand "))}`)
+
+  const project = await p.group(
+    {
+      type: ({ results }) =>
+        p.select({
+          message: `Pick a project template"`,
+          initialValue: "dapp",
+          options: [
+            { value: "vite-wagmi-starter", hint: "react" },
+            { value: "webext-dapp-template", hint: "react" },
+            { value: "vite-homepage-seed", hint: "vue" },
+            { value: "astro-seed", hint: "todo" },
+          ],
+        }),
+      path: () =>
+        p.text({
+          message: "Where should we create your project?",
+          placeholder: "your-dapp",
+          validate: (value) => {
+            if (!value) return "Please enter a path."
+          },
+        }),
+      install: () =>
+        p.confirm({
+          message: "Install dependencies?",
+          initialValue: false,
+        }),
     },
-    async (argv) => {
-      let workspace = argv.workspace
-      const projects = await getModuleProjects()
-      const workspaces = projects.map((project) => {
-        return {
-          name: project.name,
-          message: `${project.name}(${project.description})`,
-        }
-      })
-      const isWorkspaceValid =
-        workspace && workspaces.find((project) => project.name === workspace)
-
-      if (!isWorkspaceValid) {
-        workspace && console.log(chalk.red.bold(`${workspace} is not exist\n`))
-
-        if (!workspace) {
-          const { project } = await prompt<{
-            project: string
-          }>({
-            type: "select",
-            name: "project",
-            message: "Please Select",
-            choices: workspaces,
-          })
-          workspace = project
-        }
-      }
-      // make fisand.json
-      const fisandConfFile = path.join(cwd, CONFIG_FILE)
-      const confTemplate = {
-        config: {
-          workspace,
-        },
-      }
-
-      fs.writeFileSync(fisandConfFile, JSON.stringify(confTemplate, null, 2))
-
-      console.log(chalk.green(`Init ${CONFIG_FILE} success`))
+    {
+      onCancel: () => {
+        p.cancel("Operation cancelled.")
+        process.exit(0)
+      },
     },
   )
-  .command(
-    "import [-d] [-r] <name..>",
-    "Import source code",
-    async (_) => {
-      _.positional("name", {
-        type: "string",
-        describe: "Source name",
-      })
-      _.positional("directory", {
-        type: "string",
-        describe: "Download path",
-        alias: "d",
-      })
-      _.positional("ref", {
-        type: "string",
-        describe: "Source version: branch/commit/tag",
-        alias: "r",
-        default: "master",
-      })
-    },
-    async (
-      argv: yargs.ArgumentsCamelCase<{
-        name: string[]
-        r?: string
-        ref?: string
-        d?: string
-        directory: string
-      }>,
-    ) => {
-      const fisandComps = await checkFisandComp()
-      argv.name.forEach((n) => {
-        if (!fisandComps.includes(n)) {
-          console.log(
-            chalk.red.bold(`${n} is not exist\nUse fisand list to check`),
-          )
-          process.exit(1)
-        }
-      })
 
-      // The name of branch, tag or commit
-      let ref = argv.r || argv.ref || "master"
-      const fisandConfFile = path.join(cwd, CONFIG_FILE)
-      const confTemplate: Record<string, any> = {}
-      if (!fs.existsSync(fisandConfFile)) {
-        argv.name.forEach((n) => (confTemplate[n] = ref))
-        fs.writeFileSync(fisandConfFile, JSON.stringify(confTemplate, null, 2))
-      } else {
-        const existConf = require(fisandConfFile)
-        if (existConf && existConf.config && existConf.config.workspace) {
-          root = path.join(cwd, existConf.config.workspace)
-        }
-        // get workspace
-        argv.name.forEach((n) => {
-          if (typeof existConf[n] === "string") {
-            ref = existConf[n]
-          } else {
-            existConf[n] = ref
-            fs.writeFileSync(fisandConfFile, JSON.stringify(existConf, null, 2))
-          }
-        })
-      }
+  const s = p.spinner()
+  s.start(`Download template ${project.type}`)
+  await runCommand(`npx degit fisand/${project.type} ${project.path}`)
+  s.stop("Download complete")
 
-      const notBoolean = (...b: unknown[]) =>
-        b.every((_) => typeof _ !== "boolean")
-      // dest
-      if ((argv.d || argv.directory) && notBoolean(argv.d, argv.directory)) {
-        root = path.join(cwd, argv.d || argv.directory)
-      }
+  if (project.install) {
+    const s = p.spinner()
+    s.start("Installing via pnpm")
+    await runCommand(`cd ./${project.path} && pnpm i`)
+    s.stop("Installed via pnpm")
+  }
 
-      // order -d > fisand.json workspace > fisand-modules
-      console.log(chalk.green(`Download source to ${root}`))
+  let nextSteps = `cd  ${project.path}     \n${
+    project.install ? "" : "pnpm install\n"
+  }pnpm dev`
 
-      if (!fs.existsSync(root)) {
-        fs.mkdirSync(`${root}`, { recursive: true })
-      }
+  p.note(nextSteps, "next:")
 
-      for (const i of argv.name.map((n) =>
-        downloadFisand(n, notBoolean(ref) ? ref : "master"),
-      )) {
-        // download source
-        const depMap = await i
+  p.outro(`${color.green("Success!")}`)
+}
 
-        // search deps
-        const deps = Object.keys(depMap)
-        const workspace = (require(fisandConfFile).config || {}).workspace || ""
-        if (workspace) {
-          for (const dep of deps) {
-            if (dep.startsWith(workspace)) {
-              const name = dep.replace(`${workspace}/`, "")
-              // check deps then download
-              if (!fs.existsSync(`${root}/${name}`)) {
-                await downloadFisand(name, (depMap[dep] as string) || "master")
-              }
-            }
-          }
-        }
-      }
-
-      process.exit(0)
-    },
-  )
-  .example("$0 list", chalk.bold.greenBright("List all available sources"))
-  .help()
-  .alias("help", "h")
-  .version()
-  .alias("version", "v").argv
+main().catch(console.error)
